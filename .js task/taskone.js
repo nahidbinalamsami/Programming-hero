c420d7d83a9923b91f43c33e77b1bf774f7b70f4
nahidbinalamsami <saminahidbinalam@gmail.com>
2024-01-30 22:15:13 +0600

    my website

---
+++ .js task/taskone.js
@@ -0,0 +1,5 @@
+var totalMoney = 1000;
+var orange = 200;
+var apple = 100;
+console .log(orange+apple)
+console .log (totalMoney-300)
---
+++ .js task/tasktwo.js
@@ -0,0 +1,8 @@
+var mathmatics = parseFloat('75.25')
+var biology = 65;
+var chemistry = 80;
+var physics = parseFloat('35.45')
+var bangla = parseFloat('99.50')
+console .log(mathmatics+biology+chemistry+physics+bangla)
+console .log(355/5)
+console .log (355%5 tofixed(two))
